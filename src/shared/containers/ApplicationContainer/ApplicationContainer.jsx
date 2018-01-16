7b6a2a0a02439e4286d537be347133e43895963f
Rafael Hernandez <rafaelhernandez@nypl.org>
2018-01-16 15:16:10 -0500

    Added sidebar component as sateful component

--- src/shared/containers/ApplicationContainer/ApplicationContainer.jsx
+++ src/shared/containers/ApplicationContainer/ApplicationContainer.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Sidebar from '../Sidebar/Sidebar';
+import Sidebar from '../../components/Sidebar/Sidebar';
 
 class ApplicationContainer extends React.Component {
   render() {
     return (
       <div id="recap-admin" className="recap-admin">
-        <div><h1>Header</h1></div>
-        <div>
-          Sidebar goes here
-        </div>
+        <Sidebar />
         {this.props.children}
-        <div><h2>Footer</h2></div>
       </div>
     );
   }
