92540b74efcaafe4fb56be5d674f858eabec2480
Rafael Hernandez <rafaelhernandez@nypl.org>
2018-01-20 14:06:15 -0500

    updated the update-metadata form to include dynamic barcodes input fields

--- src/shared/containers/UpdateMetadata/UpdateMetadata.jsx
+++ src/shared/containers/UpdateMetadata/UpdateMetadata.jsx
@@ -6,14 +6,20 @@ class UpdateMetadata extends Component {
     super(props);
     this.state = {
       type: 'transfer-barcode',
+      barcodes: [
+        {
+          value: ''
+        }
+      ],
       barcode: '',
       customerCode: '',
       bibRecordNumber: '',
-      cgdProtectionEnabled: true
+      protectCGD: true
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.addBarcodeField = this.addBarcodeField.bind(this);
   }
 
   handleInputChange(event) {
@@ -25,30 +31,90 @@ class UpdateMetadata extends Component {
   }
 
   handleFormSubmit() {
+    console.log(this.state);
+  }
 
+  renderFormField({ inputRef, ...rest }) {
+    return (
+      <input ref={inputRef} {...rest} />
+    );
+  }
+
+  handleBarcodesInputChange(index) {
+    return (evt) => {
+      const updatedBarcodes = this.state.barcodes.map((barcode, idx) => {
+        if (index !== idx) {
+          return barcode;
+        }
+
+        return { ...barcode, value: evt.target.value };
+      });
+
+      this.setState({ barcodes: updatedBarcodes });
+    }
+  }
+
+  addBarcodeField() {
+    this.setState({ barcodes: this.state.barcodes.concat([{ value: '' }]) });
+  }
+
+  removeBarcodeField(index) {
+    this.setState({ barcodes: this.state.barcodes.filter((elem, idx) => index !== idx ) });
   }
 
   renderTransferBarcodeForm() {
     return (
       <form onSubmit={this.handleFormSubmit}>
-        <label>
-          Barcode
+        {this.state.barcodes.map((barcode, index) => (
+          <li key={index}>
+            <input
+              type="text"
+              placeholder={index > 0 ? `Barcode #${index + 1}` : 'Barcode'}
+              value={barcode.value}
+              onChange={this.handleBarcodesInputChange(index)}
+            />
+            {
+              (index > 0) &&
+              <button type="button" onClick={this.removeBarcodeField.bind(this, index)}>-</button>
+            }
+          </li>
+        ))}
+        {this.state.barcodes.length < 10 &&
+          <button type="button" onClick={this.addBarcodeField}>
+            Add New Barcode
+          </button>
+        }
+        <div>
+          <label htmlFor="barcode">Barcode</label>
           <input
+            id="barcode"
             name="barcode"
             type="text"
             value={this.state.barcode}
             onChange={this.handleInputChange}
           />
-        </label>
-        <label>
-          Barcode
+        </div>
+        <div>
+          <label htmlFor="customerCode">Customer Code</label>
           <input
-            name="barcode"
+            id="customerCode"
+            name="customerCode"
             type="text"
-            value={this.state.barcode}
+            value={this.state.customerCode}
             onChange={this.handleInputChange}
           />
-        </label>
+        </div>
+        <div>
+          <label htmlFor="protectCGD">Protect CGD</label>
+          <input
+            id="protectCGD"
+            name="protectCGD"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <input type="submit" value="Submit" />
       </form>
     );
   }
