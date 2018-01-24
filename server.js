e40db466177bccef177ddde9d7288aca9023a3ea
Stephen Betts <thisisstephenbetts@gmail.com>
2018-01-24 10:05:09 -0500

    Have a working implementation of the SSO protection. Does not yet redirect back to the appropriate URL after login.

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import path from 'path';
 import express from 'express';
+import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import compress from 'compression';
 import colors from 'colors';
+import passport from 'passport';
+import { OAuth2Strategy } from 'passport-oauth';
+import { ensureLoggedIn } from 'connect-ensure-login';
+import jwt_decode from 'jwt-decode';
 // App Route Handling
 import { initializeTokenAuth } from './src/server/routes/auth';
 import { getPatronData } from './src/server/routes/api';
@@ -29,8 +34,68 @@ app.set('port', process.env.PORT || appConfig.port);
 app.use(cookieParser());
 // Set Global publicKey
 app.set('nyplPublicKey', appConfig.publicKey);
+app.use(session({ secret: 'gregoneillbringstheparty' }));
 // Sets the server path to /dist
 app.use(express.static(distPath));
+// Use passport middleware for authentication
+app.use(passport.initialize());
+// use passport sessions
+app.use(passport.session());
+
+// Setup OAuth2 authentication
+// Protect all routes, except the auth provider
+app.use(function (req, res, next) {
+  if (req.originalUrl.match(/(?:\/auth\/provider|callback)(?:\?.*)?$/)) {
+    console.info('not checking logged in status for request url:', req.originalUrl);
+    return next();
+  }
+  console.info('checking logged in status for request url:', req.originalUrl);
+  return ensureLoggedIn('/auth/provider')(req, res, next);
+});
+
+passport.use('provider', new OAuth2Strategy(
+  {
+    authorizationURL: 'https://isso.nypl.org/oauth/authorize',
+    tokenURL: 'https://isso.nypl.org/oauth/token',
+    clientID: 'platform_admin',
+    clientSecret: 'cc7ea675154a927f013739a6dc598985e5a08cfc',
+    callbackURL: 'http://local.nypl.org/callback',
+    state: true
+  },
+  function(accessToken, refreshToken, profile, done) {
+    const {name, email, user_id} = jwt_decode(accessToken);
+    const user = {name, email, user_id}
+    
+    console.log('User decoded in callback:', user);
+    
+    done(null, user);
+  }
+));
+
+// Serializer and deserializer for app user
+passport.serializeUser(function(user, done) {
+  console.log('Serializing', user);
+  done(null, user.user_id);
+});
+passport.deserializeUser(function(user_id, done) {
+  console.log('Deserializing', user_id);
+  done(null, {user_id});
+});
+
+// Redirect the user to the OAuth 2.0 provider for authentication.  When
+// complete, the provider will redirect the user back to the application at
+//     /auth/provider/callback
+app.get('/auth/provider', passport.authenticate('provider', {scope: ['openid', 'login:staff']}));
+
+// The OAuth 2.0 provider has redirected the user back to the application.
+// Finish the authentication process by attempting to obtain an access
+// token.  If authorization was granted, the user will be logged in.
+// Otherwise, authentication has failed.
+app.get('/callback',
+  passport.authenticate('provider', { successRedirect: '/',
+                                      failureRedirect: '/auth/provider'}));
+
+
 // Establishes all application routes handled by react-router
 app.get('*', renderAdminView);
 //app.get('*', initializeTokenAuth, getPatronData, renderAdminView);
@@ -55,7 +120,7 @@ const gracefulShutdown = () => {
     console.log('Closed out remaining connections.');
     process.exit(0);
   });
-  // if after
+  // if after 
   setTimeout(() => {
     console.error('Could not close connections in time, forcefully shutting down');
     process.exit();
