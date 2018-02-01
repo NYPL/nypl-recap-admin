a075e1ccbc11fcfd6c692a6913964959126097bd
Rafael Hernandez <rafaelhernandez@nypl.org>
2018-02-01 16:40:17 -0500

    updated back-end logic to handle sqs data on a single route using higher order function composition to pass sqsClient

--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import aws from 'aws-sdk';
+import isEmpty from 'lodash/isEmpty';
 import config from '../../../../config/appConfig';
 
 function constructApiHeaders(token = '') {
@@ -18,49 +18,76 @@ function getSqsPayload(object) {
   };
 }
 
+function isSqsDataValid(params, type) {
+  const { barcodes, protectCGD, action, email } = params;
+  if (type === 'transfer') {
+    const { bibRecordNumber } = params;
 
-export function updateMetadata(req, res, next) {
-  const params = req.body;
-
-  if (!params) {
-    return res.status(400).json({
-      error: 'Error: malformed request; verify payload properties',
-      status: 400
-    });
+    if (isEmpty(barcodes) || isEmpty(bibRecordNumber) || typeof protectCGD !== 'boolean' || isEmpty(action) || isEmpty(email)) {
+      return false;
+    }
   }
 
-  const paramsAsJsonString = JSON.stringify(params);
-  const { api , region } = config.sqs;
-  aws.config.update({ region: region });
-  // Send to SQS
-  const sqsPayload = getSqsPayload({ message: paramsAsJsonString, queueUrl: api });
-  const sqs = new aws.SQS({ apiVersion: '2012-11-05' });
+  if (type === 'update') {
+    if (isEmpty(barcodes) || typeof protectCGD !== 'boolean' || isEmpty(action) || isEmpty(email)) {
+      return false;
+    }
+  }
 
-  sqs.sendMessage(sqsPayload, (err, data) => {
-    if (err) {
-      console.log("Error", err);
+  return true;
+}
+
+export function handleSqsDataProcessing(sqsClient, type) {
+  return (req, res, next) => {
+    const { api } = config.sqs;
+    const params = req.body;
+
+    if (!sqsClient) {
       return res.status(503).json({
-        error: err,
+        error: 'SQS Client is not defined',
         status: 503
       });
     }
 
-    return res.json({
-      status: 200,
-      response: data
+    if (!type || type === '') {
+      return res.status(503).json({
+        error: 'SQS transaction type is undefined',
+        status: 503
+      });
+    }
+
+    if (!params) {
+      return res.status(400).json({
+        error: 'Error: SQS payload data is undefined',
+        status: 400
+      });
+    }
+
+
+    if (isSqsDataValid(params, type) === false) {
+      return res.status(400).json({
+        error: 'Error: SQS required payload properties are invalid',
+        debugInfo: { payload: params, type: type },
+        status: 400
+      });
+    }
+
+    const paramsAsJsonString = JSON.stringify(params);
+    const sqsPayload = getSqsPayload({ message: paramsAsJsonString, queueUrl: api });
+
+    sqsClient.sendMessage(sqsPayload, (err, data) => {
+      if (err) {
+        console.log("Error", err);
+        return res.status(503).json({
+          error: err,
+          status: 503
+        });
+      }
+
+      return res.json({
+        status: 200,
+        response: data
+      });
     });
-  });
-}
-
-export function transferMetadata(req, res, next) {
-  const params = req.body;
-
-  if (!params) {
-    return res.status(400).json({
-      error: 'Error: malformed request; verify payload properties',
-      status: 400
-    });
-  }
-
-  console.log(params);
+  };
 }
