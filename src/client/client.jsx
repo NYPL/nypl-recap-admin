4d203effb1f419d96eb57307b576b1be68d64e1f
Kang Peng <kangtingpeng@nypl.org>
2018-03-08 17:09:38 -0500

    Removed redux related files.

--- src/client/client.jsx
+++ src/client/client.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
-import { Provider } from 'react-redux';
 // React Router & App Routes
 import BrowserRouter from 'react-router-dom/BrowserRouter';
 import { renderRoutes } from 'react-router-config';
 import routes from '../shared/routes';
-
-import configureStore from '../shared/store/configureStore';
 // Styles
 import './styles/main.scss';
 
-// Initialize store
-const store = configureStore();
-
-console.log(store);
-
 const Root = ({ store }) => {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        {renderRoutes(routes)}
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      {renderRoutes(routes)}
+    </BrowserRouter>
   );
 }
 
@@ -30,9 +20,7 @@ window.onload = () => {
 
   hydrate(
     <BrowserRouter>
-      <Provider store={store}>
-        {renderRoutes(routes)}
-      </Provider>
+      {renderRoutes(routes)}
     </BrowserRouter>,
     document.getElementById('nypl-recap-admin')
   );
