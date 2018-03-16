97e982cc4a46c2e3cbde429a19c75c45ca4a02d0
Kang Peng <kangtingpeng@nypl.org>
2018-03-16 16:55:41 -0400

    Added the values for the column 'NYPL Item?'

--- src/shared/containers/Refile/RefileErrorsForm.jsx
+++ src/shared/containers/Refile/RefileErrorsForm.jsx
@@ -345,6 +345,16 @@ class RefileErrorsForm extends Component {
         const afMessages = (afMessageArray.length) ?
           map(afMessageArray, (message, i) => <span className="af-message" key={i}>{message.replace(/^\"|\"$/g, '')}</span>): null;
 
+        let isNyplItem = 'yes';
+
+        const sip2Response = (item.sip2Response) ? JSON.parse(item.sip2Response) : null;
+        const CR = (sip2Response && sip2Response.variable.CR && sip2Response.variable.CR.length) ?
+          sip2Response.variable.CR[0].trim() :undefined;
+
+        if (CR == 'os') {
+          isNyplItem = 'no'
+        }
+
         return (
             <tr key={i}>
               <td>{item.id}</td>
@@ -352,7 +362,7 @@ class RefileErrorsForm extends Component {
               <td>{(item.updatedDate) ? item.createdDate.split('T')[0] : ''}</td>
               <td>{(item.updatedDate) ? item.updatedDate.split('T')[0] : ''}</td>
               <td className="af-message-td">{afMessages}</td>
-              <td></td>
+              <td>{isNyplItem}</td>
             </tr>
           );
         }
